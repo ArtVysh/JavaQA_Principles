4a18b870f08101ddceaf31705b1fae6ac142414d
ArtVysh <vysh-artem@yandex.ru>
2023-10-12 20:38:46 +0400

    Added birthday field

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        FormDate birthday = new FormDate();
-        birthday.day = 13;
-        birthday.month = 6;
-        birthday.year = 1999;
-
         Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
         post.name = "Иван";
         post.passport = "4444 № 44444444";
         post.patronymic = "Иванович";
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,4 +1,5 @@
 public class Post {
+    public FormDate birthday;
     public String name;
     public String passport;
     public String patronymic;
